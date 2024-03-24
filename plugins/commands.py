5996f5da7f01e3f27d199a91d72ec6b6da6616cd
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-03-24 23:22:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,31 +18,19 @@ from plugins.functions.forcesub import handle_force_subscribe
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
     if not update.from_user:
-        return await update.reply_text("I don't know about you sar :(")
+        return await update.reply_text("I don't know about you sir :(")
     await add_user_to_database(bot, update)
     await bot.send_message(
         Config.LOG_CHANNEL,
-        f"#NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!",
-        disable_web_page_preview=True  # Optional parameter to disable web page preview
+           f"#NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
     )
-
-    # Correct indentation here
+    
     if Config.UPDATES_CHANNEL:
-        fsub = await handle_force_subscribe(bot, update)
-        if fsub == 400:
-            return
-
-    # Send the text message
-    start_message = await bot.send_message(
-        chat_id=update.chat.id,
+      fsub = await handle_force_subscribe(bot, update)
+      if fsub == 400:
+        return
+    await update.reply_text(
         text=Translation.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=Translation.START_BUTTONS
-    )
-
-    # Send the photo as a separate message
-    await bot.send_photo(
-        chat_id=update.chat.id,
-        photo="https://graph.org/file/3914354a76ff708b3983f.jpg",  # Replace with the actual URL of your image
-        reply_to_message_id=start_message.message_id  # Reply to the start message
     )
